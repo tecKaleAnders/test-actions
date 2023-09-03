b8d6cba8050f9285f4de44c74bfd06e2f94f76dd
tecKaleAnders <143652890+tecKaleAnders@users.noreply.github.com>
2023-09-03 10:39:09 -0500

    fix live

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,5 +15,5 @@ import * as logger from "firebase-functions/logger";
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase! this is a deploy today");
 });
